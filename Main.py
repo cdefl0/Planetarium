8fa4722d42a5e30d6e72b6b4de179efca2f58412
cdefl0 <60612704+cdefl0@users.noreply.github.com>
2020-02-27 09:48:18 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,4 +2,3 @@ from Planetarium import Planetarium
 
 print ("enter a planet number (1 = mercury, 2 = venus, 3 = earth, etc.")
 
-if 
--- Planetarium.py
+++ Planetarium.py
@@ -1,9 +1,44 @@
-class Planetarium(object):
-
-
 class Planet(object):
     string name
     int num
-    double dist
-    string bio
-    
+    string dist
+
+one = Planet()
+name = "mercury"
+num = 1
+dist = "33.942 million miles"
+
+one = Planet()
+name = "venus"
+num = 2
+dist = "33.942 million miles"
+
+one = Planet()
+name = "earth"
+num = 3
+dist = "33.942 million miles"
+
+one = Planet()
+name = "mars"
+num = 4
+dist = "33.942 million miles"
+
+one = Planet()
+name = "jupiter"
+num = 5
+dist = "33.942 million miles"
+
+one = Planet()
+name = "saturn"
+num = 6
+dist = "33.942 million miles"
+
+one = Planet()
+name = "uranus"
+num = 7
+dist = "33.942 million miles"
+
+one = Planet()
+name = "neptune"
+num = 8
+dist = "33.942 million miles"
