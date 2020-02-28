f2b2090c87489b8f7813093ce75e217ad59d47f6
cdefl0 <60612704+cdefl0@users.noreply.github.com>
2020-02-28 08:34:02 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,4 +1,72 @@
-from Planetarium import Planetarium
-
 print ("enter a planet number (1 = mercury, 2 = venus, 3 = earth, etc.")
 
+int x = input
+
+class Planet(object):
+    string name
+    int num
+    string dist
+
+one = Planet()
+name = "mercury"
+num = 1
+dist = "33.942 million miles"
+
+two = Planet()
+name = "venus"
+num = 2
+dist = "33.942 million miles"
+
+three = Planet()
+name = "earth"
+num = 3
+dist = "33.942 million miles"
+
+four = Planet()
+name = "mars"
+num = 4
+dist = "33.942 million miles"
+
+five = Planet()
+name = "jupiter"
+num = 5
+dist = "33.942 million miles"
+
+six = Planet()
+name = "saturn"
+num = 6
+dist = "33.942 million miles"
+
+seven = Planet()
+name = "uranus"
+num = 7
+dist = "33.942 million miles"
+
+eight = Planet()
+name = "neptune"
+num = 8
+dist = "33.942 million miles"
+
+if (x == 1):
+    print (one.name, one.num, one.dist)
+
+if (x == 2):
+    print (two.name, two.num, two.dist)
+
+if (x == 3):
+    print (three.name, three.num, three.dist)
+
+if (x == 4):
+    print (four.name, four.num, four.dist)
+
+if (x == 5):
+    print (five.name, five.num, five.dist)
+
+if (x == 6):
+    print (six.name, six.num, six.dist)
+
+if (x == 7):
+    print (seven.name, seven.num, seven.dist)
+
+if (x == 8):
+    print (eight.name, eight.num, eight.dist)
